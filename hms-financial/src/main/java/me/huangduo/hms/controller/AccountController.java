c916edcde7d52609aff464991c23c22180927133
duo huang <duo.huang@thoughtworks.com>
2024-12-28 21:21:00 +0800

    add logback

--- hms-financial/src/main/java/me/huangduo/hms/controller/AccountController.java
+++ hms-financial/src/main/java/me/huangduo/hms/controller/AccountController.java
@@ -1,5 +1,6 @@
 package me.huangduo.hms.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/accounts")
+@Slf4j
 public class AccountController {
 
 
     @GetMapping
     public List<String> getUsers() {
+        log.info("get account...");
         return Arrays.asList("huang duo", "zhang san");
     }
 }

0db4aa1bb5ece89080374c70b79f3ec0a248c862
duo huang <duo.huang@thoughtworks.com>
2025-02-14 17:53:57 +0800

    refactor MessageServiceImpl

--- hms-notification/src/main/java/me/huangduo/hms/service/MessageServiceImpl.java
+++ hms-notification/src/main/java/me/huangduo/hms/service/MessageServiceImpl.java
@@ -16,6 +16,7 @@ import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,9 +25,7 @@ import java.util.stream.Stream;
 public class MessageServiceImpl implements MessageService {
 
     private final SinksManager sinksManager;
-
     private final MessagesDao messagesDao;
-
     private final MessageMapper messageMapper;
 
     public MessageServiceImpl(EventHandlerService eventHandlerService, SinksManager sinksManager, MessagesDao messagesDao, MessageMapper messageMapper) {
@@ -34,86 +33,85 @@ public class MessageServiceImpl implements MessageService {
         this.messagesDao = messagesDao;
         this.messageMapper = messageMapper;
 
-//        EventHandler genericHandler = event -> this.sinksManager.emit(event.getMessage());
+        registerEventHandlers(eventHandlerService);
+    }
 
-        // register event handler
-        eventHandlerService.registerEventHandler(InvitationEvent.class, evt -> {
-            InvitationEvent.InvitationMessagePayload messagePayload = (InvitationEvent.InvitationMessagePayload) evt.getMessage().getDeserializedPayload();
-            sinksManager.sendDirectMessage(messagePayload.getReceiver().getUserId(), evt.getMessage());
-        });
-        eventHandlerService.registerEventHandler(NotificationEvent.class, evt -> {
-            NotificationEvent.NotificationMessagePayload messagePayload = (NotificationEvent.NotificationMessagePayload) evt.getMessage().getDeserializedPayload();
-            sinksManager.sendHomeMessage(messagePayload.getHomeId(), evt.getMessage());
-        });
-
+    private void registerEventHandlers(EventHandlerService eventHandlerService) {
+        eventHandlerService.registerEventHandler(InvitationEvent.class, this::handleInvitationEvent);
+        eventHandlerService.registerEventHandler(NotificationEvent.class, this::handleNotificationEvent);
         eventHandlerService.registerEventHandler(BroadcastEvent.class, evt -> sinksManager.sendBroadcastMessage(evt.getMessage()));
+    }
+
+    private void handleInvitationEvent(HmsEvent evt) {
+        InvitationEvent.InvitationMessagePayload messagePayload =
+                (InvitationEvent.InvitationMessagePayload) evt.getMessage().getDeserializedPayload();
+        sinksManager.sendDirectMessage(messagePayload.getReceiver().getUserId(), evt.getMessage());
+    }
+
+    private void handleNotificationEvent(HmsEvent evt) {
+        NotificationEvent.NotificationMessagePayload messagePayload =
+                (NotificationEvent.NotificationMessagePayload) evt.getMessage().getDeserializedPayload();
+        sinksManager.sendHomeMessage(messagePayload.getHomeId(), evt.getMessage());
     }
 
     @Override
     public Flux<Message> getLiveMessage(User userInfo) {
-
         return sinksManager.getFlux(userInfo.getUserId())
-                .doOnCancel(() -> {
-                    log.info("doOnCancel - remove skin: {}", userInfo.getUserId());
-                    sinksManager.removeSkin(userInfo.getUserId());
-                })
-                .doOnComplete(() -> {
-                    log.info("doOnComplete - remove skin: {}", userInfo.getUserId());
-                    sinksManager.removeSkin(userInfo.getUserId());
-                })
-                .doOnTerminate(() -> {
-                    log.info("doOnTerminate - remove skin: {}", userInfo.getUserId());
-                    sinksManager.removeSkin(userInfo.getUserId());
-                }).map(message -> {
-                    if (message.getMessageType() == MessageTypeEnum.INVITATION) {
-                        HmsEvent.MessagePayload messagePayload = message.getDeserializedPayload();
-                        if (messagePayload instanceof InvitationEvent.InvitationMessagePayload invitationMessagePayload) {
-                            if (Objects.equals(userInfo.getUserId(), invitationMessagePayload.getReceiver().getUserId())) {
-                                message.setMessageContent(String.format("%s 邀请你加入他的家庭", invitationMessagePayload.getPublisher().getNickname()));
-                            }
+                .doOnCancel(() -> cleanUpUser(userInfo))
+                .doOnComplete(() -> cleanUpUser(userInfo))
+                .doOnTerminate(() -> cleanUpUser(userInfo))
+                .map(message -> enrichMessage(userInfo, message));
+    }
 
-                            if (Objects.equals(userInfo.getUserId(), invitationMessagePayload.getPublisher().getUserId())) {
-                                message.setMessageContent(String.format("你已邀请 %s 加入家庭", invitationMessagePayload.getReceiver().getNickname()));
-                            }
-                        }
+    private void cleanUpUser(User userInfo) {
+        log.info("Cleanup - remove skin: {}", userInfo.getUserId());
+        sinksManager.removeSkin(userInfo.getUserId());
+    }
 
-                    }
-                    return message;
-                });
+    private Message enrichMessage(User userInfo, Message message) {
+        if (message.getMessageType() == MessageTypeEnum.INVITATION) {
+            Optional<InvitationEvent.InvitationMessagePayload> payload =
+                    extractInvitationPayload(message);
+            payload.ifPresent(invitationMessagePayload -> updateMessageContent(userInfo, message, invitationMessagePayload));
+        }
+        return message;
+    }
+
+    private Optional<InvitationEvent.InvitationMessagePayload> extractInvitationPayload(Message message) {
+        HmsEvent.MessagePayload messagePayload = message.getDeserializedPayload();
+        return messagePayload instanceof InvitationEvent.InvitationMessagePayload
+                ? Optional.of((InvitationEvent.InvitationMessagePayload) messagePayload)
+                : Optional.empty();
+    }
+
+    private void updateMessageContent(User userInfo, Message message, InvitationEvent.InvitationMessagePayload invitationMessagePayload) {
+        if (Objects.equals(userInfo.getUserId(), invitationMessagePayload.getReceiver().getUserId())) {
+            message.setMessageContent(String.format("%s 邀请你加入他的家庭", invitationMessagePayload.getPublisher().getNickname()));
+        } else if (Objects.equals(userInfo.getUserId(), invitationMessagePayload.getPublisher().getUserId())) {
+            message.setMessageContent(String.format("你已邀请 %s 加入家庭", invitationMessagePayload.getReceiver().getNickname()));
+        }
     }
 
     @Override
     public List<Message> getHistoryMessages(User userInfo, Integer homeId) {
         // directed message & home message & broadcast message
+        List<MessageEntity> directedMessages = messagesDao.getHistoryMessagesByReceiveUserId(userInfo.getUserId());
+        List<MessageEntity> otherMessages = getOtherMessages(homeId);
 
-        List<MessageEntity> directedMessage = messagesDao.getHistoryMessagesByReceiveUserId(userInfo.getUserId());
-        List<MessageEntity> restMessages;
-
-        if (homeId == 0) {
-            restMessages = messagesDao.getHistoryMessagesByType(MessageTypeEnum.BROADCAST);
-        } else {
-            restMessages = messagesDao.getHistoryMessagesByTypeOrHomeId(MessageTypeEnum.BROADCAST, homeId);
-        }
-
-
-        return Stream.concat(directedMessage.stream(), restMessages.stream()).map(x -> {
-            Message message = messageMapper.toModel(x);
-
-            if (message.getMessageType() == MessageTypeEnum.INVITATION) {
-                HmsEvent.MessagePayload messagePayload = message.getDeserializedPayload();
-                if (messagePayload instanceof InvitationEvent.InvitationMessagePayload invitationMessagePayload) {
-                    if (Objects.equals(userInfo.getUserId(), invitationMessagePayload.getReceiver().getUserId())) {
-                        message.setMessageContent(String.format("%s 邀请你加入他的家庭", invitationMessagePayload.getPublisher().getNickname()));
-                    }
-
-                    if (Objects.equals(userInfo.getUserId(), invitationMessagePayload.getPublisher().getUserId())) {
-                        message.setMessageContent(String.format("你已邀请 %s 加入家庭", invitationMessagePayload.getReceiver().getNickname()));
-                    }
-                }
-
-            }
-            return message;
-        }).sorted((o1, o2) -> o2.getCreatedAt().compareTo(o1.getCreatedAt())).collect(Collectors.toList());
+        return Stream.concat(directedMessages.stream(), otherMessages.stream())
+                .map(x -> mapToModelWithEnrichment(userInfo, x))
+                .sorted((o1, o2) -> o2.getCreatedAt().compareTo(o1.getCreatedAt()))
+                .collect(Collectors.toList());
     }
 
+    private List<MessageEntity> getOtherMessages(Integer homeId) {
+        return homeId == 0
+                ? messagesDao.getHistoryMessagesByType(MessageTypeEnum.BROADCAST)
+                : messagesDao.getHistoryMessagesByTypeOrHomeId(MessageTypeEnum.BROADCAST, homeId);
+    }
+
+    private Message mapToModelWithEnrichment(User userInfo, MessageEntity entity) {
+        Message message = messageMapper.toModel(entity);
+        return enrichMessage(userInfo, message);
+    }
 }

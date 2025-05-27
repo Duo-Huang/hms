27daec97c7ffd1b6a37b78711a4fd6a8c2c3fda6
duo huang <duo.huang@thoughtworks.com>
2025-05-27 16:49:05 +0800

    fix username validation

--- hms-user/src/main/java/me/huangduo/hms/dto/request/UserRegistrationRequest.java
+++ hms-user/src/main/java/me/huangduo/hms/dto/request/UserRegistrationRequest.java
@@ -1,5 +1,6 @@
 package me.huangduo.hms.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -7,11 +8,11 @@ import me.huangduo.hms.enums.ErrorCodeEnum;
 
 public record UserRegistrationRequest(
         @NotEmpty(message = "username can not be empty.")
-        @Size(max = 16, message = "username must be at most 16 characters long.")
+        @Size(min = 6, max = 16, message = "username must be between 6 and 16 characters.")
         @Pattern(regexp = "^[a-zA-Z0-9_-]+$", message = "username can only contain letters, numbers, underscores, and hyphens.")
         String username,
 
-        @NotEmpty(message = "password can not be empty.")
+        @NotBlank(message = "password can not be empty.")
         @Size(min = 6, max = 20, message = "The password length must be between 6 and 20 characters.")
         String password
 ) implements HmsRequestBody {
